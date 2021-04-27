6a5435609f6077b78f7a507d6798aa3dd0ae4157
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-27 12:45:08 +0300

    Add polls | templates | static files |

--- super_news/super_news/urls.py
+++ super_news/super_news/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from polls.views import index_handler
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', index_handler),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
