9332eb8ba37c75af06aa2102021986ae915f5e12
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-05-08 12:18:34 +0300

    Add models for news app /  Make migrate / Add in admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Author, Article, Comment, Newsletter, Tag
+
+
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Newsletter)
+admin.site.register(Tag)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    in_menu = models.BooleanField(default=True)
+    order = models.IntegerField(default=1)
+
+
+class Author(models.Model):
+    name = models.TextField(max_length=255)
+    bio = models.TextField(max_length=255)
+    avatar = models.ImageField()
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    content = models.TextField()
+    short_description = models.TextField()
+    main_image = models.ImageField()
+    pub_date = models.DateField(auto_now_add=True)
+    categories = models.ManyToManyField(Category)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    """ author связан со статьёй, если удалить автора, то и статья удляется.
+        То есть при такой связи тот, у которого указывается, он зависит от того, на которого ссылается. 
+    """
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    website = models.CharField(max_length=255)
+    comment = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+class Newsletter(models.Model):
+    email = models.EmailField()
+    is_active = models.BooleanField(default=True)
+    subscribe_date = models.DateTimeField(auto_now_add=True)
+    unsubscribe_date = models.DateTimeField(null=True, blank=True)
+    """
+    null=True, blank=True - чтобы пустое было изначально
+    """
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    articles = models.ManyToManyField(Article)
