6b1d6548965a3f1d5732dfa78ac9951fc026aba3
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-06-30 16:28:30 +0300

    Static root without base_dir

--- super_news/settings.py
+++ super_news/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static_root/')
+STATIC_ROOT = '/static_root/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
