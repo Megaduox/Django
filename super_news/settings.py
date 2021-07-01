20c61c972c963a995513a8857e77ccc81c8bbf8d
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-07-01 11:40:56 +0300

    Delete STATICFILES_DIRS

--- super_news/settings.py
+++ super_news/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/static_root/")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_FINDERS = (
 'django.contrib.staticfiles.finders.FileSystemFinder',
